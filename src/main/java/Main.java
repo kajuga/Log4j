a57f8f91794cd2f04d77dcceb3a31ab1206e7e66
kajuga <kajuga@mail.ru>
2019-04-13 18:36:17 +0300

    git init

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    private static OrderLogic logic;
+
+    public static void main(String[] args) {
+        logic = new OrderLogic();
+        logic.doOrder();
+    }
+}
---
+++ src/main/java/OrderLogic.java
@@ -0,0 +1,20 @@
+import org.apache.log4j.Logger;
+
+public class OrderLogic {
+    private static final Logger log = Logger.getLogger(OrderLogic.class);
+
+    public void doOrder(){
+        // какае-то логика
+        System.out.println("Заказ оформлен!");
+        log.info("Это информационное сообщение!");
+        addToCart();
+    }
+
+    private void addToCart() {
+        // добавление товара в корзину
+        System.out.println("Товар добавлен в корзину");
+        // логируем ошибку
+        log.error("Это сообщение ошибки");
+    }
+
+}
